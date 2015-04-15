512d6edc8b469575d095c61c78c76fa5cdcedbda
gonzalonunez <gonzi@tcpmiami.com>
2015-04-15 08:41:39 -0400

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Gonzalo Nunez on 4/15/15.
+//  Copyright (c) 2015 Gonzalo Nunez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
